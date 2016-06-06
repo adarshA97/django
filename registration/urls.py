c82bdb92ac6364bfbcf168bc065207f9b8a2de01
adarshA97 <adarsh.ashok100@gmail.com>
2016-06-06 12:46:14 +0530

    6commit

--- registration/urls.py
+++ registration/urls.py
@@ -4,4 +4,7 @@ from registration.views import *
 
 urlpatterns = [
 url(r'^$', UserRegistrationView.as_view(), name='register_user'),
+    url(r'^user/success/',TemplateView.as_view(template_name='success.html'),
+        name='page'),
+    url( r'^chocolate/add/', AddChocolateView.as_view(), name="add_choclate")
     ]
